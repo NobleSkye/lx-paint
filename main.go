14229462165e04c4d1cdb6687d80361b294aa1b4
Skylar Pearl <rainblueskylar@gmail.com>
2024-09-02 14:35:58 -0700

    built

--- main.go
+++ main.go
@@ -2,35 +2,32 @@ package main
 
 import (
 	"image/color"
+	"log"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{120, 180, 255, 255})
+	ebitenutil.DebugPrint(screen, "Stopping Dev for now but will pick back up soon (09/2/24)")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return ebiten.WindowSize()
+}
+
 func main() {
-	// Create a new application
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Hello World Window")
-
-	// Set the background color to blue using the color package
-	background := canvas.NewRectangle(color.NRGBA{R: 0, G: 0, B: 255, A: 255}) // Blue color
-
-	// Create a label with "Hello, World!"
-	label := widget.NewLabel("Hello, World!")
-	label.Alignment = fyne.TextAlignCenter // Center align the text
-
-	// Create a container with the background and label
-	content := container.NewMax(background, label)
-
-	// Set the content of the window
-	myWindow.SetContent(content)
-
-	// Set window size
-	myWindow.Resize(fyne.NewSize(300, 200))
-
-	// Show and run the application
-	myWindow.ShowAndRun()
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Lx Paint")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
